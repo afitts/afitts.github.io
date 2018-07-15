70f3de1b5da0d0a4b6368071ef81c2d49402b957
afitts <fitts.alex@gmail.com>
2018-07-15 09:34:01 -0500

    Revert "commit message"
    This reverts commit 8bfc0b46c93ea6d844debc1160bbf431a9792cc1.
    
    Reverting to 1/25/17 commit as all the markdown pages disappeared for some reason

---
+++ content/2016/06/hi.md
@@ -0,0 +1,8 @@
+Title: Hello Universe
+Date: 2016-06-10
+Slug: hello-world
+
+Hello UNIVERSE. This is Alex Fitts reporting in. 
+
+This space will serve as a hub for all the data science and programming projects I plan to embark on. I may occaisionally dip into astronomy, as is my nature, but hopefully I can find a wonderful marriage between the two. But who knows what we will find out there? Our only choice to venture forth and find out!  
+
---
+++ pelicanconf.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Alex Fitts'
+SITENAME = u'Alex Fitts'
+SITEURL = 'http://localhost:8000'#'http://www.gregreda.com'
+TIMEZONE = 'America/Chicago'
+THEME = 'void/'
+AVATAR = '/theme/images/avatar.jpg'
+TITLE = "Alex Fitts: independent data science and strategy consulting."
+DESCRIPTION = "Alex Fitts is a curious astronomy who has wandered into \
+the world of data science. What started out as a way to better understand\
+astronomy has taken a life of its own."
+
+# TODO: switch to /blog/slug/index.html -- need to setup redirects first
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+# Static Pages
+PAGE_PATHS = ['pages']
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+ABOUT_PAGE_HEADER = 'Per aspera ad astra.'
+
+# DEFAULTS
+DEFAULT_LANG = 'en'
+DEFAULT_CATEGORY = 'misc'
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%b %d, %Y'
+DEFAULT_PAGINATION = False
+
+# FEEDS
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+TAG_FEED_ATOM = "feeds/tag/%s.atom.xml"
+
+# PLUGINS
+PLUGIN_PATHS = ['pelican-plugins', 'pelican_dynamic']
+PLUGINS = ['assets', 'liquid_tags.notebook', 'pelican_dynamic', 'render_math']
+
+CODE_DIR = 'code'
+NOTEBOOK_DIR = 'notebooks'
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+
+STATIC_PATHS = ['images', 'code', 'notebooks', 'extra', 'data']
+EXTRA_PATH_METADATA = {'extra/robots.txt': {'path': 'robots.txt'},}
+
+# TODO: NAVBAR - make it dynamic
+NAVIGATION = [
+    (),
+]
+
+# TODO: SOCIAL - make it dynamic
+TWITTER_CARDS = True
+TWITTER_NAME = "Alex_Fitts"
+FACEBOOK_SHARE = True
+GITHUB_NAME = 'lcdmsoundsystem'
+LINKEDIN_URL = 'http://linkedin.com/in/afitts'
+GOOGLE_PLUS_URL = 'https://plus.google.com/u/0/117088506946894057793/about/p/pub'
+LASTFM_NAME = 'gjreda'
+
+#### Analytics
+GOOGLE_ANALYTICS = 'UA-34295039-1'
+DOMAIN = "gregreda.com"
+
+# Other
+MAILCHIMP = False
+CACHE_CONTENT = False
+AUTORELOAD_IGNORE_CACHE = True
